836795ede8bb27fa7a6201019dec4a486e0bfaac
Ma Phu Dien <DienMa@MacBook-Pro-cua-Ma.local>
2019-10-21 12:01:28 +0700

    A

---
+++ app_calender_todolist/app/src/main/java/com/example/calendar_app/Custom_Calendar.java
@@ -0,0 +1,17 @@
+package com.example.calendar_app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class Custom_Calendar extends LinearLayout {
+    public Custom_Calendar(Context context) {
+        super(context);
+    }
+
+    public Custom_Calendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
